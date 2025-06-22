910bbebacd8654c6be7e229577d2d248d890b45f
MatheusfcRodrigues <matheuscrodrigues9@outlook.com>
2025-06-22 14:30:54 -0300

    Final Commit

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90ccd152b40d03781313e15dd1b249722a15721")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d47fac8425d32eba65206bce8e45fed5a6da5b68")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
